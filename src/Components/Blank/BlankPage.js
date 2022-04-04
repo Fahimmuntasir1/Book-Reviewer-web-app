e69f3ad9de64418024cfeddd01ab511e6f1751fe
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-04-04 16:24:01 +0600

    blankPage / not found page create

--- src/Components/Blank/BlankPage.js
+++ src/Components/Blank/BlankPage.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React from "react";
+import "./BlankPage.css";
 
 const blankPage = () => {
-    return (
-        <div>
-            <h2>404</h2>
-        </div>
-    );
+  return (
+    <div className="blank">
+      <h2 className="text-6xl font-semibold pt-5">Oops!</h2> <br />
+      <h2 className="text-5xl">404</h2>
+      <p className=" text-3xl mt-2 pb-4">Page Not Found !</p>
+    </div>
+  );
 };
 
 export default blankPage;
