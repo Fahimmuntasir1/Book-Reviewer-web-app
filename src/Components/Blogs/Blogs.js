1b94c9aaa9cc2ff5977e1706909086c0bc1fbb6b
Fahimmuntasir1 <fahimmuntasir043@gmail.comm>
2022-04-05 14:47:02 +0600

    qna sec added

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -8,10 +8,25 @@ const Blogs = () => {
         <li className="text-2xl text-left ml-5">
           What is context api ? Purpose of using context api?
         </li>
+        <p>
+          The Context API is a React structure that enables you to exchange
+          unique details and assists in solving prop-drilling from all levels of
+          your application.This is the alternative to "prop drilling" or moving
+          props from grandparent to child to parent, and so on. The purpose of
+          this it provides a way to share values like these between components
+          without having to separately pass a prop through every level of the
+          tree. So we can easily do this.
+        </p>
         <li className="text-2xl text-left ml-5">What is Semantic Tag?</li>
+        <p>
+          {" "}
+          Semantic tags provides meaning to an HTML page rather than just
+          presentation.A semantic element clearly describes its meaning to both the browser and the developer.It makes HTML more comprehensible by better defining the different sections and layout of web pages.Semantic HTML tags allow you to add meaning to your markup so that search engines, screen readers, and web browsers can make sense of it.So we must should use Semantic tag in our Website.
+        </p>
         <li className="text-2xl text-left ml-5">
           About inline-block ,Inline-block-element.
         </li>
+        <p>Inline-block elements are similar to inline elements, except they can have padding and margins added on all four sides.</p>
       </div>
     </div>
   );
